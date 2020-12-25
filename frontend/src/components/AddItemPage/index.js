4f820f1ec8c40c353994f5d672d81da89336e59e
Andrew Dotterer <adotterer@gmail.com>
2020-12-25 14:09:01 -0800

    add css @ make buttons for price - and + buttons

--- frontend/src/components/AddItemPage/index.js
+++ frontend/src/components/AddItemPage/index.js
@@ -2,13 +2,13 @@ import React, { useState } from "react";
 import * as itemActions from "../../store/items";
 import { useDispatch, useSelector } from "react-redux";
 // import { Redirect } from "react-router-dom";
-// import "./LoginForm.css";
+import "./addItemPage.css";
 
 function AddItemPage() {
   const dispatch = useDispatch();
   // const sessionUser = useSelector((state) => state.session.user);
   const [itemName, setItemName] = useState("");
-  const [itemPrice, setItemPrice] = useState("");
+  const [itemPrice, setItemPrice] = useState(0.0);
   const [itemImage, setItemImage] = useState("");
   const [itemDescription, setItemDescription] = useState("");
   // const [credential, setCredential] = useState("");
@@ -32,63 +32,99 @@ function AddItemPage() {
     // TODO: ADD ERROR HANDLING
   };
 
+  
+
   return (
-    <form onSubmit={handleSubmit}>
-      <ul>
-        {errors.map((error, idx) => (
-          <li key={idx}>{error}</li>
-        ))}
-      </ul>
-      <label>
-        Item Name
+    <div className="div__container">
+      <form onSubmit={handleSubmit}>
+        <ul>
+          {errors.map((error, idx) => (
+            <li key={idx}>{error}</li>
+          ))}
+        </ul>
+        <label>
+          <h3>Item Name</h3>
+          <div>
+            <input
+              className="input__box"
+              type="text"
+              value={itemName}
+              onChange={(e) => setItemName(e.target.value)}
+              required
+            />
+          </div>
+        </label>
+        <label>
+          <h3>Price</h3>
+          <br />
+          <div className="div__inline">
+            <button
+              className="button__inline"
+              onClick={(e) => {
+                e.preventDefault();
+                let newItemPrice = itemPrice > 0 ? itemPrice - 1 : itemPrice;
+                setItemPrice(newItemPrice);
+              }}
+            >
+              -
+            </button>
+            <div className="div__price">
+              <input
+                className="input__price"
+                type="number"
+                min="1"
+                step="1"
+                value={itemPrice}
+                onChange={(e) => setItemPrice(e.target.value)}
+                required
+              />
+            </div>
+            <button
+              className="button__inline"
+              onClick={(e) => {
+                e.preventDefault();
+                let newItemPrice =
+                  itemPrice < 10000 ? itemPrice + 1 : itemPrice;
+                setItemPrice(newItemPrice);
+              }}
+            >
+              +
+            </button>
+          </div>
+        </label>
+        <br />
         <div>
-          <input
-            type="text"
-            value={itemName}
-            onChange={(e) => setItemName(e.target.value)}
-            required
-          />
+          <label>
+            <h3>Upload Image</h3>
+            <div>
+              <input
+                type="file"
+                value={itemImage}
+                onChange={(e) => setItemImage(e.target.value)}
+                accept="image/x-png,image/gif,image/jpeg"
+                required
+              />
+            </div>
+          </label>
         </div>
-      </label>
-      <label>
-        Price
+        <label>
+          <h3>Description</h3>
+          <div>
+            <textarea
+              className="input__box"
+              value={itemDescription}
+              onChange={(e) => setItemDescription(e.target.value)}
+              required
+            />
+          </div>
+        </label>
         <div>
-          <input
-            type="number"
-            min="0.01"
-            step="0.01"
-            value={itemPrice}
-            onChange={(e) => setItemPrice(e.target.value)}
-            required
-          />
+          <button className="button__full-length" type="submit">
+            Submit
+          </button>
         </div>
-      </label>
-      <label>
-        Upload Image
-        <div>
-          <input
-            type="file"
-            value={itemImage}
-            onChange={(e) => setItemImage(e.target.value)}
-            accept="image/x-png,image/gif,image/jpeg"
-            required
-          />
-        </div>
-      </label>
-      <label>
-        Description
-        <div>
-          <textarea
-            value={itemDescription}
-            onChange={(e) => setItemDescription(e.target.value)}
-            required
-          />
-        </div>
-      </label>
-      <div>
-        <button type="submit">Submit</button>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 }
 
