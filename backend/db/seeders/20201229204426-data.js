a25ba54f1a3a02e25276186d4715f0d1e19dbfd2
Andrew Dotterer <adotterer@gmail.com>
2020-12-29 16:03:09 -0800

    item-image associations added to seed data

--- backend/db/seeders/20201229204426-data.js
+++ backend/db/seeders/20201229204426-data.js
@@ -68,25 +68,46 @@ module.exports = {
       { returning: true }
     );
 
+    // ...incase the seed messes up the primary key numbering...
+    const userIdArr = users.map((obj) => {
+      return obj.id;
+    });
+
     const randomUser = () => {
       return Math.floor(Math.floor(users.length) * Math.random());
     };
 
     const items = await queryInterface.bulkInsert(
       "Items",
+      // KEEP IN ALPHABETICAL ORDER!
       [
+        {
+          itemName: "Avocados",
+          description:
+            "My avocado tree just produces too much fruit. There's only so much avocado toast I can eat! Come get them for free!",
+          price: null,
+          sellerId: userIdArr[randomUser()],
+          location: "Seattle, WA",
+        },
+        {
+          itemName: "Bird Eye chili pepper (one bunch) !spicy!",
+          description: lorem.generateParagraphs(3),
+          price: 4,
+          sellerId: userIdArr[randomUser()],
+          location: "Seattle, WA",
+        },
+        {
+          itemName: "Cilantro",
+          description: lorem.generateParagraphs(3),
+          price: null,
+          sellerId: userIdArr[randomUser()],
+          location: "Seattle, WA",
+        },
         {
           itemName: "Croton",
           description: lorem.generateParagraphs(4),
           price: 10,
-          sellerId: randomUser(),
-          location: "Seattle, WA",
-        },
-        {
-          itemName: "Rosemary plant, small",
-          description: lorem.generateParagraphs(3),
-          price: 3,
-          sellerId: randomUser(),
+          sellerId: userIdArr[randomUser()],
           location: "Seattle, WA",
         },
         {
@@ -94,120 +115,128 @@ module.exports = {
           description:
             "Act fast, or forever be basic... get your fiddle-leaf fig before they all sell out.",
           price: 200,
-          sellerId: randomUser(),
-          location: "Seattle, WA",
-        },
-        {
-          itemName: "Variegated monstera",
-          description:
-            "CHA-CHING there goes your stimulus check-- but at least you will have one of our variegated monsteras! ",
-          price: 500,
-          sellerId: randomUser(),
-          location: "Seattle, WA",
-        },
-        {
-          itemName: "Avocados",
-          description:
-            "My avocado tree just produces too much fruit. There's only so much avocado toast I can eat! Come get them for free!",
-          price: null,
-          sellerId: randomUser(),
+          sellerId: userIdArr[randomUser()],
           location: "Seattle, WA",
         },
         {
           itemName: "Lemons",
           description: lorem.generateParagraphs(3),
           price: null,
-          sellerId: randomUser(),
+          sellerId: userIdArr[randomUser()],
+          location: "Seattle, WA",
+        },
+        {
+          itemName: "Lettuce",
+          description: lorem.generateParagraphs(3),
+          price: 1,
+          sellerId: userIdArr[randomUser()],
+          location: "Seattle, WA",
+        },
+        {
+          itemName: "Rosemary plant, small",
+          description: lorem.generateParagraphs(3),
+          price: 8,
+          sellerId: userIdArr[randomUser()],
           location: "Seattle, WA",
         },
         {
           itemName: "Thai basil cutting",
           description: lorem.generateParagraphs(3),
           price: null,
-          sellerId: randomUser(),
+          sellerId: userIdArr[randomUser()],
           location: "Seattle, WA",
         },
         {
-          itemName: "Cilantro",
-          description: lorem.generateParagraphs(3),
-          price: null,
-          sellerId: randomUser(),
-          location: "Seattle, WA",
-        },
-        {
-          itemName: "Bird Eye chili pepper (one bunch) !spicy!",
-          description: lorem.generateParagraphs(3),
-          price: 4,
-          sellerId: randomUser(),
+          itemName: "Variegated monstera",
+          description:
+            "CHA-CHING there goes your stimulus check-- but at least you will have one of our variegated monsteras!",
+          price: 500,
+          sellerId: userIdArr[randomUser()],
           location: "Seattle, WA",
         },
       ],
       { returning: true }
     );
-    const images = await queryInterface.bulkInsert("Images", [
-      {
-        // AVOCADOS
-        URL:
-          "https://green-grab-bucket.s3-us-west-1.amazonaws.com/avocados.jpg",
-      },
-      {
-        // BIRDS EYE CHILIS
-        URL:
-          "https://green-grab-bucket.s3-us-west-1.amazonaws.com/bird_eyes_chili_pepper.jpeg",
-      },
-      {
-        // CILANTRO
-        URL:
-          "https://green-grab-bucket.s3-us-west-1.amazonaws.com/cilantro.jpeg",
-      },
-      {
-        // CROTON
-        URL: "https://green-grab-bucket.s3-us-west-1.amazonaws.com/croton.jpg",
-      },
-      {
-        // FIDDLE LEAF
-        URL:
-          "https://green-grab-bucket.s3-us-west-1.amazonaws.com/fiddle-leaf-fig.jpeg",
-      },
-      {
-        // LEMONS
-        URL: "https://green-grab-bucket.s3-us-west-1.amazonaws.com/lemons.jpg",
-      },
-      {
-        // LETTUCE
-        URL:
-          "https://green-grab-bucket.s3-us-west-1.amazonaws.com/lettuce.jpeg",
-      },
-      {
-        // ROSEMARY
-        URL:
-          "https://green-grab-bucket.s3-us-west-1.amazonaws.com/rosemary.png",
-      },
-      {
-        // THAI BASIL
-        URL:
-          "https://green-grab-bucket.s3-us-west-1.amazonaws.com/thai_basil.jpeg",
-      },
-      {
-        // VARIEGATED MONSTERA
-        URL:
-          "https://green-grab-bucket.s3-us-west-1.amazonaws.com/variegated_monstera.jpeg",
-      },
-    ]);
-    const itemImageAssociations = await queryInterface.bulkInsert(
-      "ItemImageAssociations",
+
+    const images = await queryInterface.bulkInsert(
+      "Images",
       [
-        // avocados.jpg
+        // KEEP IN ALPHABETICAL ORDER!
         {
-          itemId,
-          imageId,
-          primaryId,
+          // AVOCADOS
+          URL:
+            "https://green-grab-bucket.s3-us-west-1.amazonaws.com/avocados.jpg",
         },
-      ]
+        {
+          // BIRDS EYE CHILIS
+          URL:
+            "https://green-grab-bucket.s3-us-west-1.amazonaws.com/bird_eyes_chili_pepper.jpeg",
+        },
+        {
+          // CILANTRO
+          URL:
+            "https://green-grab-bucket.s3-us-west-1.amazonaws.com/cilantro.jpeg",
+        },
+        {
+          // CROTON
+          URL:
+            "https://green-grab-bucket.s3-us-west-1.amazonaws.com/croton.jpg",
+        },
+        {
+          // FIDDLE LEAF
+          URL:
+            "https://green-grab-bucket.s3-us-west-1.amazonaws.com/fiddle-leaf-fig.jpeg",
+        },
+        {
+          // LEMONS
+          URL:
+            "https://green-grab-bucket.s3-us-west-1.amazonaws.com/lemons.jpg",
+        },
+        {
+          // LETTUCE
+          URL:
+            "https://green-grab-bucket.s3-us-west-1.amazonaws.com/lettuce.jpeg",
+        },
+        {
+          // ROSEMARY
+          URL:
+            "https://green-grab-bucket.s3-us-west-1.amazonaws.com/rosemary.png",
+        },
+        {
+          // THAI BASIL
+          URL:
+            "https://green-grab-bucket.s3-us-west-1.amazonaws.com/thai_basil.jpeg",
+        },
+        {
+          // VARIEGATED MONSTERA
+          URL:
+            "https://green-grab-bucket.s3-us-west-1.amazonaws.com/variegated_monstera.jpeg",
+        },
+      ],
+      { returning: true }
     );
+
+    // The images and items only line up correctly if the seed data is all in order
+
+    const itemImgArrFunc = () => {
+      const arr = [];
+      for (let i = 0; i < items.length; i++) {
+        arr.push({
+          itemId: items[i].id,
+          imageId: images[i].id,
+          primaryId: images[i].id,
+        });
+      }
+      return arr;
+    };
+
+    const itemImgArr = itemImgArrFunc();
+    // console.log("itemImageArr", itemImageArr);
+    await queryInterface.bulkInsert("ItemImageAssociations", itemImgArr);
   },
 
   down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete("ItemImageAssociations");
     await queryInterface.bulkDelete("Items");
     await queryInterface.bulkDelete("Users");
     /*
